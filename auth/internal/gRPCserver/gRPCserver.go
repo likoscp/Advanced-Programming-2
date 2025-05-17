464d9ada4f66a921270dd054c3295883091aae60
barcek2281 <sabdpp17@gmail.com>
2025-05-17 18:22:46 +0500

    xzczxvzvcxz

--- auth/internal/gRPCserver/gRPCserver.go
+++ auth/internal/gRPCserver/gRPCserver.go
@@ -4,9 +4,11 @@ import (
 	"context"
 	"errors"
 	"log/slog"
+	"time"
 
 	authv1 "github.com/barcek2281/finalProto/gen/go/auth"
 	"github.com/likoscp/Advanced-Programming-2/auth/internal/configs"
+	"github.com/likoscp/Advanced-Programming-2/auth/internal/lib/jwt"
 	"github.com/likoscp/Advanced-Programming-2/auth/internal/repository"
 	"github.com/likoscp/Advanced-Programming-2/auth/internal/store/postgresql"
 	"github.com/likoscp/Advanced-Programming-2/auth/models"
@@ -39,22 +41,23 @@ func (g *GRPCserver) Register(ctx context.Context, in *authv1.RegisterRequest) (
 		Password: in.GetPassword(),
 	}
 
-	if !user.IsValid() {
+	if err := user.IsValid(); err != nil {
 		slog.Warn("invalid email")
-		return nil, ErrNotValidEmail
+		return nil, err
 	}
-	if !user.IsValidPassword() {
+	if err := user.HashPassword(); err != nil {
 		slog.Warn("invalid password")
-
-		return nil, ErrNotValidPassword
+		return nil, err
 	}
 
 	id, err := g.authRepository.Create(user)
-
 	if err != nil {
 		slog.Error("error to create user", "err", err)
-
 		return nil, err
 	}
-	return &authv1.RegisterResponse{Token: id}, nil
+	user.ID = id
+
+	token, err := jwt.NewToken("SEX", *user, time.Hour*24)
+
+	return &authv1.RegisterResponse{Token: token}, nil
 }
--- auth/models/user.go
+++ auth/models/user.go
@@ -1,14 +1,32 @@
 package models
 
+import (
+	"github.com/go-playground/validator"
+	"golang.org/x/crypto/bcrypt"
+)
+
 type User struct {
-	Email    string
-	Password string
+	ID       string
+	Password string `validate:"required,email"`
+	Email    string `validate:"required,min=5,max=25"`
 }
 
-func (u *User) IsValid() bool {
-	return true
+func (u *User) IsValid() error {
+	validate := validator.New()
+
+	err := validate.Struct(u)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
-func (u *User) IsValidPassword() bool {
-	return true
+func (u *User) HashPassword() error {
+
+	bytes, err := bcrypt.GenerateFromPassword([]byte(u.Password), 14)
+	if err != nil {
+		return err
+	}
+	u.Password = string(bytes)
+	return nil
 }
